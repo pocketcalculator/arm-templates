a78cc57e0b36dc8952334cf1ca468b4ca3795289
Paul Sczurek <pocketcalculatorshow@gmail.com>
2021-07-16 17:11:45 -0400

    bastion working

---
+++ pocketcalculatorshow/bastion/bastion-azure-arm.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+subscription=''
+region=eastus2
+application=pocketcalculatorshow
+environment=dev
+owner=paul.sczurek@outlook.com
+resourceGroupName=rg-$application-$environment-$region
+vnetName=vnet-$application-$environment-$region
+vnetCIDRPrefix=10.0
+
+echo subscription = $subscription
+echo location = $location
+echo application = $application
+echo environment = $environment
+echo owner = $owner
+echo resourceGroupName = $resourceGroupName
+echo vnetName = $vnetName
+echo vnetCIDRPrefix = $vnetCIDRPrefix
+
+vmName=vm-bastion-$application-$environment-$region
+adminUsername=pocketcalculatorshow
+adminPasswordOrKey=@p0ck3tcalculat0rshow!
+subnetName=snet-public-0
+networkSecurityGroupName=sg-bastion
+
+echo vmName = $vmName
+echo adminUsername = $adminUsername
+echo adminPasswordOrKey = $adminPasswordOrKey
+echo subnetName = $subnnetName
+echo networkSecurityGroupName = $networkSecurityGroupName
+
+echo "Creating deployment for ${environment} ${application} bastion..."
+az deployment group create \
+	--resource-group $resourceGroupName \
+	--name $vnetName-deployment \
+	--template-file ./bastion.json \
+	--parameters \
+		"location=$region" \
+	        "adminUsername=$adminUsername" \
+       		"adminPasswordOrKey=$adminPasswordOrKey" \
+        	"vmName=$vmName" \
+        	"vnetName=$vnetName"
+echo "Deployment for ${environment} ${application} bastion is complete."
